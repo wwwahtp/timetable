7b0ddf068431cf11b34c6c98fe8970f98afb896e
wwwahtp <wwwmoses@g.skku.edu>
2023-08-01 23:24:07 +0900

    initial

---
+++ src/AddPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function AddPage() {
+  return <div>d</div>;
+}
+export default AddPage;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
+import AddPage from './AddPage';
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/AddPage" element={<AddPage />}></Route>
+      </Routes>
+      <div className="App">
+        <div className="screen">
+          <div className="title">
+            <h1>타임테이블</h1>
+          </div>
+
+          <div className="tt-grid">
+            <Gen date="a" />
+            <Gen date="b" />
+            <Gen date="c" />
+            <Gen date="d" />
+            <Gen date="e" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
+function Gen(props) {
+  const times = [
+    '9시~10시',
+    '10시~11시',
+    '11시~12시',
+    '12시~13시',
+    '13시~14시',
+    '14시~15시',
+    '15시~16시',
+    '16시~17시',
+    '17시~18시',
+  ];
+  function hello() {
+    alert('1');
+  }
+
+  return (
+    <>
+      {times.map((con, idx) => (
+        <div
+          className="element"
+          onClick={Navigate('/AddPage')}
+          id={props.date + idx}
+        >
+          {con}
+        </div>
+      ))}
+    </>
+  );
+}
 export default App;
